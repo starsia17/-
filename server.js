5e6fdfe107c315ced3213d6bdf4af08d877711cb
starsia17 <128087226+starsia17@users.noreply.github.com>
2026-07-09 03:20:55 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,64 +4,89 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const mongoose = require('mongoose');
 
-// 데이터베이스 연결
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('데이터베이스 연결 성공!'))
-  .catch(err => console.error('데이터베이스 연결 실패:', err));
+  .then(() => console.log('DB 연결 성공'))
+  .catch(err => console.error('DB 연결 실패:', err));
 
-// 데이터 모델 정의
 const Room = mongoose.model('Room', new mongoose.Schema({
     roomName: String,
-    creator: String
+    creator: String,
+    password: { type: String, default: '' },
+    announcement: { type: String, default: '' },
+    poll: { question: String, options: [String], votes: [Number] }
 }));
 
 const Message = mongoose.model('Message', new mongoose.Schema({
     room: String,
     username: String,
     text: String,
+    isImage: { type: Boolean, default: false },
     timestamp: { type: Date, default: Date.now }
 }));
 
-// 소켓 통신 로직
 io.on('connection', (socket) => {
-    
-    // 1. 접속하자마자 현재 존재하는 방 목록을 전달
-    Room.find().then(rooms => {
-        socket.emit('initialRooms', rooms);
-    });
+    Room.find().then(rooms => socket.emit('initialRooms', rooms));
 
-    // 2. 방 만들기 요청 처리
     socket.on('createRoom', async (data) => {
-        const newRoom = new Room({ roomName: data.roomName, creator: data.nickname });
+        const newRoom = new Room({ roomName: data.roomName, creator: data.nickname, password: data.password });
         await newRoom.save();
-        // 모든 사용자에게 새 방이 생겼음을 알림
-        io.emit('roomCreated', newRoom); 
+        io.emit('roomCreated', newRoom);
     });
 
-    // 3. 방 입장 처리
-    socket.on('joinRoom', (roomName) => {
-        socket.join(roomName);
+    socket.on('joinRoom', async (data) => {
+        const room = await Room.findOne({ roomName: data.roomName });
+        if (room && (!room.password || room.password === data.password)) {
+            socket.join(data.roomName);
+            socket.emit('joinResult', { success: true, roomData: room });
+        } else {
+            socket.emit('joinResult', { success: false, message: '비밀번호 틀림' });
+        }
     });
 
-    // 4. 채팅 메시지 저장 및 전송
+    // 공지사항 작성 (방장 확인)
+    socket.on('setAnnouncement', async (data) => {
+        const room = await Room.findOne({ roomName: data.roomName });
+        if (room && room.creator === data.nickname) {
+            room.announcement = data.text;
+            await room.save();
+            io.to(data.roomName).emit('updateAnnouncement', data.text);
+        }
+    });
+
+    // 투표 생성 (방장 확인)
+    socket.on('createPoll', async (data) => {
+        const room = await Room.findOne({ roomName: data.roomName });
+        if (room && room.creator === data.nickname) {
+            room.poll = { question: data.question, options: data.options, votes: new Array(data.options.length).fill(0) };
+            await room.save();
+            io.to(data.roomName).emit('updatePoll', room.poll);
+        }
+    });
+
+    // 투표하기
+    socket.on('vote', async (data) => {
+        const room = await Room.findOne({ roomName: data.roomName });
+        if (room && room.poll) {
+            room.poll.votes[data.optionIndex]++;
+            await room.save();
+            io.to(data.roomName).emit('updatePoll', room.poll);
+        }
+    });
+
     socket.on('chat message', async (msg) => {
-        const newMessage = new Message({
-            room: msg.room,
-            username: msg.username,
-            text: msg.text
-        });
+        const newMessage = new Message({ room: msg.room, username: msg.username, text: msg.text, isImage: msg.isImage });
         await newMessage.save();
-        // 해당 방에 있는 사람들에게만 메시지 전송
         io.to(msg.room).emit('chat message', msg);
     });
+
+    socket.on('deleteRoom', async (data) => {
+        const room = await Room.findOne({ roomName: data.roomName });
+        if (room && room.creator === data.nickname) {
+            await Room.deleteOne({ roomName: data.roomName });
+            io.emit('roomDeleted', data.roomName);
+        }
+    });
 });
 
-// 기본 경로 설정
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
-// 서버 실행
-http.listen(process.env.PORT || 3000, () => {
-    console.log('서버가 성공적으로 시작되었습니다.');
-});
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+http.listen(process.env.PORT || 3000, () => console.log('서버 실행 중'));
